bc4d8cc07a933e6b6707d4ffb9e9c268edab3337
Victor Chung <kindlychung@gmail.com>
2014-06-03 17:32:41 +0200

    ...

--- bin2int.pro
+++ bin2int.pro
@@ -2,6 +2,14 @@ TEMPLATE = app
 CONFIG += console
 CONFIG -= app_bundle
 CONFIG -= qt
+QMAKE_CC = clang
+QMAKE_CXX = clang++
+QMAKE_CXXFLAGS += -std=c++11
+QMAKE_CXXFLAGS += -D_FILE_OFFSET_BITS=64
+INCLUDEPATH += /usr/include/c++/4.8
+INCLUDEPATH += /usr/include/boost
+LIBS += -lboost_system
+LIBS += -lboost_filesystem
 
 SOURCES += main.cpp
 
--- main.cpp
+++ main.cpp
@@ -1,10 +1,39 @@
 #include <iostream>
+#include <fstream>
+#include <boost/filesystem.hpp>
 
 using namespace std;
 
-int main()
+int main(int argc, char* argv[])
 {
-    cout << "Hello World!" << endl;
+    // ===========================================
+    std::string helpmsg = R"raweos(
+Description:
+Reads a file byte by byte and print each byte as an int (from 0 to 255)
+
+Usage:
+bin2int FILE
+
+Args:
+FILE    the file you want to read
+-h      print this help message
+    )raweos";
+    // ===========================================
+
+    if(string(argv[1]) == "-h" or string(argv[1]) == "--help") {
+        cout << helpmsg << "\n";
+        exit(0);
+    }
+
+    using namespace boost::filesystem;
+    off_t f_size = file_size(argv[1]);
+    ifstream fh(argv[1], ios_base::binary);
+    unsigned char b;
+    for (off_t i = 0; i < f_size; i++) {
+        b = fh.get();
+        cout << i << " \t" << (int)b << "\n";
+    }
+
     return 0;
 }
 
